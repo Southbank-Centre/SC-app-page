f5253a965b0bc8be1385576d9c8e64a3fb647413
samueljmurray <samueljmurray@gmail.com>
2015-03-12 12:13:11 +0000

    initial commit

---
+++ release/SC-app-page.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name SC-app-page
+ * @description
+ *
+ * Provides the app with the ability to display page content and features
+ */
+angular
+  .module('SC-app-page');
